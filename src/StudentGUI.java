a7da15a94b479aa7160f31a807be28acfe1062d0
Jason <jescalante14@csub.edu>
2025-09-09 10:41:13 -0700

    Set up the first GUI, shows Hello World

---
+++ src/StudentGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class StudentGUI extends JFrame {
+    private JPanel panel1;
+
+    public StudentGUI(){
+        setContentPane(panel1);
+        setSize(500, 500 );
+    }
+}
