80de9cdde229bca26fc5dcff19a88f324eaaec11
Jason <jescalante14@csub.edu>
2025-09-09 11:13:26 -0700

    Added a list and labels for name/id as well as user inputs

--- src/StudentGUI.java
+++ src/StudentGUI.java
@@ -2,9 +2,19 @@ import javax.swing.*;
 
 public class StudentGUI extends JFrame {
     private JPanel panel1;
+    private JList studentList;
+    private JPanel studentInput;
+    private JLabel labelFIrstName;
+    private JTextField inputFirstName;
+    private JLabel labelLastName;
+    private JTextField inputLastName;
+    private JLabel labelStudentID;
+    private JTextField inputStudentID;
 
     public StudentGUI(){
         setContentPane(panel1);
         setSize(500, 500 );
+        setTitle("Student List");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
